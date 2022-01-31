908d42a0a1122dfc4586980485a242e32d401c28
HSShashank <shashank.hs547@gmail.com>
2022-01-31 12:33:52 +0530

    Added Day 5

---
+++ Day 5_ 31-01/index.js
@@ -0,0 +1,14 @@
+console.log("Simple Calculator");
+var a = 10;
+var b = 20;
+var sum = a + b;
+var diff = b - a;
+var prod = a * b;
+var quot = b / a;
+var rem = a % b;
+console.log("Sum is ", sum);
+console.log("Difference is ", diff);
+console.log("Prodcut is ", prod);
+console.log("Quotient is ", quot);
+console.log("Remainder is ", rem);
+
